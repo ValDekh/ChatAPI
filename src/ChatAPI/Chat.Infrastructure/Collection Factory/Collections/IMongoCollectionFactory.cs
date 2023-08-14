bcc8128360f6fad5c268e7353e4560f2bcae4719
ValDekh <smithdekh@gmail.com>
2023-08-14 14:33:48 +0300

    +

---
+++ src/ChatAPI/Chat.Infrastructure/Collection Factory/Collections/IMongoCollectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Infrastructure.Collection_Factory.Collections
+{
+    public interface IMongoCollectionFactory
+    {
+        ValueTask<IMongoCollection<T>> Get<T>(CancellationToken ct) where T : class, new();
+    }
+}
