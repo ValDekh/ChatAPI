21821a8e46c668dc8a428a0dec5677f80b908ceb
ValDekh <smithdekh@gmail.com>
2023-09-01 18:03:27 +0300

    Event Handling

---
+++ ChatAPI/Chat.Application/EventHandlers/ChatDeletedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Application.EventHandlers
+{
+    public class ChatDeletedEventArgs : EventArgs
+    {
+        public Guid ChatId { get; set; }
+        public ChatDeletedEventArgs(Guid chatId)
+        {
+            ChatId = chatId;
+        }
+    }
+}
---
+++ ChatAPI/Chat.Infrastructure/Services/ChatDeletedEvent.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Infrastructure.Services
+{
+    public class ChatDeletedEvent : INotification,EventArgs
+    {
+        public Guid ChatId { get; set; }
+    }
+}
