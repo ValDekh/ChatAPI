c33721eb769bff3b90ab6c1614be8953eee17aa5
ValDekh <smithdekh@gmail.com>
2023-09-07 17:53:40 +0300

    Add permission entity

---
+++ ChatAPI/Chat.Domain/Entities/Permission.cs
@@ -0,0 +1,17 @@
+﻿using Chat.Domain.Structures.Enums;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Domain.Entities
+{
+    public class Permission : BaseEntity
+    {
+        public ObjectId UserId { get; set; }
+        public ObjectId ChatId { get; set; }
+        public PermissionsEnum permissions { get; set; }
+    }
+}
